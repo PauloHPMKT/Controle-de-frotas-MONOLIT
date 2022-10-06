32c4b54e303bf72459c81b82582ddca88aac5b42
Paulo Sergio <pauloserg861@gmail.com>
2022-10-05 21:15:18 -0300

    definindo arquivos publicos

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static())
+//defining public files
+app.use(express.static(path.join(__dirname, 'public')))
 
 //backend setup
 app.use(express.urlencoded({ extended: true }))
