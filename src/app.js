b36c5056b2d1344c6f30e5bb1be0d82196875535
Paulo Sergio <pauloserg861@gmail.com>
2022-10-05 21:28:10 -0300

    criando rota para renderizacao statica

--- src/app.js
+++ src/app.js
@@ -3,8 +3,15 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+//defining static files
+app.use(express.static(path.join(__dirname, 'views')))
 //defining public files
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
 
 //backend setup
 app.use(express.urlencoded({ extended: true }))
