63c04840e34f8c53af840d132819462a2f160d1b
Paulo Sergio <pauloserg861@gmail.com>
2022-10-05 21:57:30 -0300

    instalando versao especifica do mongoose

--- src/app.js
+++ src/app.js
@@ -1,22 +1,28 @@
+//libs
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
+const db = require('./db')
+
+//express instance
 const app = express()
+
 
 //defining static files
 app.use(express.static(path.join(__dirname, 'views')))
-//defining public files
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
+//connecting database
+db.dBconnect()
 
 //backend setup
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+//rotas
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 
 module.exports = app
