6e400991df7191a860b605d960c450ae1d1762b6
Paulo Sergio <pauloserg861@gmail.com>
2022-10-26 20:44:50 -0300

    isolando estrutura de rota padrao

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,7 @@
 const router = require('express').Router()
-const userController = require('../controller/user.controller')
 const IndexController = require('../controller')
 
+//configurando rota padrão
 router.get('/', IndexController.index)
-
-/*router.get('/', (req, res) => {
-  return res.render('index', {
-    title: 'Página Inicial',
-  })
-})*/
-
-router.post('/usuario/register', userController.createUser)
 
 module.exports = router
