142d9216bc273819e712e1e0673a937d01fac661
Paulo Sergio <pauloserg861@gmail.com>
2022-10-26 21:42:48 -0300

    criando renderizacao de rotas

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,4 +3,52 @@ const UserController = require('../controller/user.controller')
 
 userRouter.post('/user/register', UserController.createUser)
 
+userRouter.get('/funcionarios', (req, res) => {
+    res.render('funcionarios', {
+        title: 'Página de funcionarios'
+    })
+})
+
+userRouter.get('/departamentos', (req, res) => {
+    res.render('departamentos', {
+        title: 'Página de departamentos'
+    })
+})
+
+userRouter.get('/veiculos', (req, res) => {
+    res.render('veiculos', {
+        title: 'Página de Veiculos'
+    })
+})
+
+userRouter.get('/motoristas', (req, res) => {
+    res.render('motoristas', {
+        title: 'Pagina de motoristas'
+    })
+})
+
+userRouter.get('/manutencoes', (req, res) => {
+    res.render('manutencoes', {
+        title: 'Pagina de manutencoes',
+    })
+})
+
+userRouter.get('/multas', (req, res) => {
+    res.render('multas', {
+        title: 'Pagina de multas',
+    })
+})
+
+userRouter.get('/solicitacoes', (req, res) => {
+    res.render('solicitacoes', {
+        title: 'Pagina de solicitacoes',
+    })
+})
+
+userRouter.get('/locacoes', (req, res) => {
+    res.render('locacoes', {
+        title: 'Pagina de Locações',
+    })
+})
+
 module.exports = userRouter
