dec9affb0b744e99157988c90c1bbdf27af5d5f4
Paulo Sergio <pauloserg861@gmail.com>
2022-10-27 22:50:10 -0300

    ajustando estrutura de rotas

--- src/controller/index.js
+++
@@ -1,9 +0,0 @@
-function index(req, res) {
-  res.render('index', {
-    title: "Página Inicial",
-  })
-}
-
-module.exports = {
-  index,
-}
--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const IndexController = require('../controller')
-
-//configurando rota padrão
-router.get('/', IndexController.index)
-
-module.exports = router
