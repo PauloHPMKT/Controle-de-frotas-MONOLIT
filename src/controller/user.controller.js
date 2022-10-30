c06c0cdaf7aebd845a4050d8934c8f1a616d8001
Paulo Sergio <pauloserg861@gmail.com>
2022-10-30 16:45:17 -0300

    implementando metodo de listagem de usuarios e rota get

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,17 @@
 const { Encript } = require('../helpers')
 const UserModel = require('../models/user.model')
 
+function getAllUsers(req, res) {
+    UserModel
+        .find()
+        .sort({ createdAt: -1 })
+        .populate('dept_id', 'name')
+        .exec((err, user) => {
+            if (err) res.status(400).send({ message: `${err.message}` })
+            else res.status(200).send(user)
+        })
+}
+
 async function createUser(req, res) {
     const { ...data } = req.body
 
@@ -13,5 +24,6 @@ async function createUser(req, res) {
 }
  
 module.exports = {
+    getAllUsers,
     createUser,
 }
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,6 +1,7 @@
 const userController = require('../controller/user.controller')
 const userRouter = require('express').Router()
 
+userRouter.get('/usuarios/listar', userController.getAllUsers)
 userRouter.post('/usuarios/register', userController.createUser)
 
 module.exports = userRouter
