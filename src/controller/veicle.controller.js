e6731f58ba69d169b28c349ce2f9d32ea5aaf0ad
Paulo Sergio <pauloserg861@gmail.com>
2022-10-30 12:07:21 -0300

    criando metodo de criacao de veiculos

--- src/controller/veicle.controller.js
+++ src/controller/veicle.controller.js
@@ -1,2 +1,14 @@
 const VeicleModel = require('../models/veicle.model')
 
+function createVeicle(req, res) {
+  const { ...data } = req.body
+
+  const veicle = new VeicleModel({ ...data })
+
+  veicle.save()
+  res.json({ veicle })
+}
+
+module.exports = {
+  createVeicle,
+}
