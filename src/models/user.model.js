f46ecfcc70c14b409d82d05ab2f8c3dea391fc67
Paulo Sergio <pauloserg861@gmail.com>
2022-10-30 11:05:42 -0300

    criando index para o modelo de banco

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    cod: Number,
+    cod: { type: Number, unique: true },
     name: String,
     email: String,
     cpf: String,
