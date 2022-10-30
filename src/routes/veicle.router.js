5abbb75e1b84d7262cd104d2c70f891a51715bf1
Paulo Sergio <pauloserg861@gmail.com>
2022-10-30 12:07:52 -0300

    criando rota de create veiculos

--- src/routes/veicle.router.js
+++ src/routes/veicle.router.js
@@ -2,4 +2,5 @@ const veicleController = require('../controller/veicle.controller')
 const veicleRouter = require('express').Router()
 
 veicleRouter.post('/veiculos/register', veicleController.createVeicle)
+
 module.exports = veicleRouter
