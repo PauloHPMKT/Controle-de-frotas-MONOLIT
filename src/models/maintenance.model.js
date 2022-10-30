401e107ade55e3ab352299cbed0706d74d5d91e0
Paulo Sergio <pauloserg861@gmail.com>
2022-10-30 13:19:53 -0300

    criando modelo de manutencao

---
+++ src/models/maintenance.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    cod: { type: Number, unique: true },
+    type: String,
+    limit_km: Number,
+    reason: String,
+    observation: String,
+    driver_id: { type: mongoose.Schema.Types.ObjectId, ref: 'usuarios' },
+    veicle_id: { type: mongoose.Schema.Types.ObjectId, ref: 'veiculos' }
+})
+
+const MaintenanceModel = mongoose.model('manutencao', schema)
+
+module.exports = MaintenanceModel
