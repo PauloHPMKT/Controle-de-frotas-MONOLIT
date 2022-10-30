e7a53060f15d6ff8fc1bb2f21c46b8c56d52c406
Paulo Sergio <pauloserg861@gmail.com>
2022-10-30 13:26:09 -0300

    criando modelo de pecas para estoque

---
+++ src/models/peaces.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    cod: { type: Number, unique: true },
+    name: String,
+    manufacturing: String,
+    quantity: Number,
+})  
+
+const PeaceModel = mongoose.model('pecas', 'schema')
+
+module.exports = PeaceModel
