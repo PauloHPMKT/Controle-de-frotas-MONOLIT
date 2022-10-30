3b1abe84cc00df592aeb720ed4fa79c55dd66105
Paulo Sergio <pauloserg861@gmail.com>
2022-10-29 23:45:33 -0300

    editando modelo de usuario

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
+    cod: Number,
     name: String,
     email: String,
+    cpf: String,
+    birthday: Date,
+    gender: String,
+    location: {
+        address: {
+            street: String,
+        },
+    },
+    dept_id: { type: mongoose.Schema.Types.ObjectId, ref: 'departamentos' },
     password: String,
+    createdAt: { type: Date, default: Date.now() },
 })
 
 const UserModel = mongoose.model('usuarios', schema)
-
 
 module.exports = UserModel
